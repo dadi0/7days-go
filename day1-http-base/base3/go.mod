17821dc5065e2d9c151159da8851debd9f320056
dadi0 <1442374919@qq.com>
2021-03-18 15:49:14 +0800

    go day2

--- day1-http-base/base3/go.mod
+++ day1-http-base/base3/go.mod
@@ -2,10 +2,6 @@ module example
 
 go 1.16
 
-require (
-	gee v0.0.0
-)
+require gee v0.0.0
 
-replace (
-	gee => ./gee
-)
+replace gee => ./gee
--- day1-http-base/base3/main.go
+++ day1-http-base/base3/main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"./gee"
 	"fmt"
+	"gee"
 	"net/http"
 )
 
-func main()  {
+func main() {
 	r := gee.New()
 	r.GET("/", func(w http.ResponseWriter, req *http.Request) {
 		fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
